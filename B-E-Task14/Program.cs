a9d416bd001c100a74be974160fb703772041ffc
suleymanibishov <suleyman22022@gmail.com>
2023-11-29 12:23:33 +0400

    Task

---
+++ B-E-Task14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace B_E_Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintText(Console.ReadLine());
+            PrintText("Salam");
+        }
+
+        static void PrintText(string text)
+        {
+            foreach (var letter in text)
+            {
+                Thread.Sleep(222);
+                Console.Write(letter);
+            }
+            Console.WriteLine();
+        }
+    }
+}
